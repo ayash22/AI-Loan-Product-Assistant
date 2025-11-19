feb3914225ddbdb967f06d1603da5f19321b43b0
AYASKANTA PARIDA <97581291+ayash22@users.noreply.github.com>
2025-11-19 23:11:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -53,10 +55,11 @@ async def ask(message: str = Form(...)):
 # Run App
 # ---------------------------------------
 if __name__ == "__main__":
-    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8002,
+        host="0.0.0.0",
+        port=port,
         reload=True
     )
+
